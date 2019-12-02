443991ced2ea77807510fe5d8a58340924028f6e
taterntots <matthew.a.weidner@gmail.com>
2019-12-02 15:09:48 -0500

    set up my App.js with a place to stire state, making it my parent component. Also added the ability to add a task here, but it is not functional yet.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import React from 'react';
 
+const tasks = [
+  {
+    name: 'Eat all the pizza',
+    id: 1
+  },
+  {
+    name: 'Walk the pupper',
+    id: 2
+  }
+]
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      tasks: tasks
+    }
+  }
+
+  addTask = newTaskText => {
+    const newTask = {
+      name: newTaskText,
+      id: Date.now(),
+    }
+    this.setState({
+      tasks: [...this.state.tasks, newTask]
+    })
+  }
+
   render() {
     return (
       <div>
